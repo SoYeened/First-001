bcc8496fd2a0af5c1b17633026bcce4d3acc9ed0
SoYeened <sergey.ryabkov@outlook.com>
2020-11-29 20:50:31 +0300

    js2

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // alert(1)
-function myAlert() {
-    let a = 1;
-    alert(a);
-}
 
-myAlert();
+// function name(count){
+//     for (let i = 0; i < 9; i++) {
+//         console.log(i);
+//     }
+// };
+
+// name(2)
+
+// function sum(a,b) {
+//     let c = a + b;
+//     return c;
+// }
+
+// let d = sum(2,2);
+// console.log(d);
+
+let a = 0;
+let b = 1;
+
+for (let i = 0; i <= 10; i++) {
+    let c = a + b;
+    console.log(a);
+    a = b;
+    b = c;
+}
